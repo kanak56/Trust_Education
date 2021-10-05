a1d98b00f0bb16ea1b763b7e41a96c3f7396e3c7
kanak56 <ckanak40@gmail.com>
2021-10-05 10:28:57 +0600

    commited

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,21 +6,25 @@ import About from '../Components/About/About';
 import { width } from '@mui/system';
 
 const Home = () => {
-    const url = 'https://thumbs.dreamstime.com/b/children-education-kid-read-book-school-boy-reading-books-dreaming-over-blackboard-background-92807607.jpg';
+    const url = 'https://images.unsplash.com/photo-1601807576163-587225545555?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1073&q=80';
     return (
         <div style={{ width: '100%' }}>
-            <Box sx={{ display: 'grid', p: 1, gridTemplateColumns: 'repeat(2, 1fr)' }} >
+            <Box sx={{ p: 1 }} >
                 <div className='homeElement'>
-                    <h1 style={{ fontFamily: 'Akronim', fontSize: 200, marginTop: 5, color: 'green', width: "100%" }}> <span style={{ fontSize: 400 }}>W</span>elcome </h1>
-                    <h2>To Trust Education Center</h2>
-                    <p style={{ fontFamily: 'Lobster', color: 'red' }}>Your trust and our resposibility</p>
-                    <p style={{ fontFamily: 'Lobster' }}>Why Programming Language is Important Nowadays</p>
-                    <p style={{ fontSize: 20 }}>Programming languages use classes and functions that control commands. The reason that programming is so important is that it directs a computer to complete these commands over and over again, so people do not have to do the task repeatedly. Instead, the software can do it automatically and accurately.</p>
+                    <div>
+                        <h1 style={{ fontFamily: 'Akronim', fontSize: 200, marginTop: 5, color: 'white', width: "100%" }}> <span style={{ fontSize: 400 }}>W</span>elcome </h1>
+                        <h2>To Trust Education Center</h2>
+                        <p style={{ fontFamily: 'Lobster', color: 'white' }}>Your trust and our resposibility</p>
+                    </div>
+                    <div className='homeinfo'>
+                        <p style={{ fontFamily: 'Lobster' }}>Why Programming Language is Important Nowadays</p>
+                        <p style={{ fontSize: 20 }}>Programming languages use classes and functions that control commands. The reason that programming is so important is that it directs a computer to complete these commands over and over again, so people do not have to do the task repeatedly. Instead, the software can do it automatically and accurately.</p>
+                    </div>
+
                 </div>
-                <div >
-                    <img src={url} alt="" srcSet='' style={{ width: '100%', height: '800px' }} />
-                </div>
+
             </Box >
+
             <br />
             <h1 style={{ fontFamily: 'Lobster', marginTop: 20, color: 'red' }}>We are Offering the best Education Services</h1>
             <br />
